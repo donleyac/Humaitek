7875e2734f1ba2283dbcbc8534d0ffcca15aa48e
donleyac <happylumberjack@gmail.com>
2016-12-08 23:46:36 -0500

    Responsive nav bar, need to add footer

--- app/components/navigation/index.js
+++ app/components/navigation/index.js
@@ -1,5 +1,7 @@
 var React = require('react');
 var LinkContainer = require('react-router-bootstrap').LinkContainer;
+var Affix = require('antd').Affix;
+
 require('./styles.scss')
 
 
@@ -7,7 +9,7 @@ var Navigation = React.createClass({
     render(){
       window.addEventListener('scroll', function(e){
         let distanceY = window.pageYOffset || document.documentElement.scrollTop;
-        let shrinkOn = 235;
+        let shrinkOn = 70;
         let navbar = document.getElementById("navbar");
         if (distanceY > shrinkOn) {
           navbar.classList.add("smaller")
@@ -20,20 +22,22 @@ var Navigation = React.createClass({
       });
       return (
         <div className="wrapper">
-          <div id="navbar">
-            <LinkContainer to="new"><div className="link line-hover"><p>Explore</p></div></LinkContainer>
-            <LinkContainer to="/">
-              <div className="logo-container">
-                <div className="logo">
-                  <svg height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
-                    <rect className="shape" height="100%" width="100%" />
-                  </svg>
-                  <p>Humaitek</p>
+          <Affix>
+            <div id="navbar">
+              <LinkContainer to="new"><div className="link line-hover"><p>Explore</p></div></LinkContainer>
+              <LinkContainer to="/">
+                <div className="logo-container">
+                  <div className="logo">
+                    <svg height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
+                      <rect className="shape" height="100%" width="100%" />
+                    </svg>
+                    <p>Humaitek</p>
+                  </div>
                 </div>
-              </div>
-            </LinkContainer>
-            <LinkContainer to="contact"><div className="link line-hover"><p>Contact</p></div></LinkContainer>
-          </div>
+              </LinkContainer>
+              <LinkContainer to="contact"><div className="link line-hover"><p>Contact</p></div></LinkContainer>
+            </div>
+        </Affix>
           {this.props.children}
         </div>
     )
