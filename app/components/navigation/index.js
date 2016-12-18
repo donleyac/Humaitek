4b84d2d3472cd44bee5f811e8156ac2b049f54b1
donleyac <happylumberjack@gmail.com>
2016-12-17 22:49:39 -0500

    Generated dist files. Updated nav to not be fixed.

--- app/components/navigation/index.js
+++ app/components/navigation/index.js
@@ -21,30 +21,28 @@ var Navigation = React.createClass({
       });
       return (
         <div className="wrapper">
-          <Affix>
-            <div id="navbar">
-              <LinkContainer to="explore"><div className="link line-hover">
-                <p>Explore</p>
-                <img className="icons" src={require('./assets/explore.png')}></img>
-              </div></LinkContainer>
-              <LinkContainer to="/">
-                <div className="logo-container">
-                  <div className="logo">
-                    <svg height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
-                      <rect className="shape" height="100%" width="100%" />
-                    </svg>
-                    <p>Humaitek</p>
-                    <img className="normal icons" src={require('./assets/home_black.png')}></img>
-                    <img className="inverse icons" src={require('./assets/home_white.png')}></img>
-                  </div>
+          <div id="navbar">
+            <LinkContainer to="explore"><div className="link line-hover">
+              <p>Explore</p>
+              <img className="icons" src={require('./assets/explore.png')}></img>
+            </div></LinkContainer>
+            <LinkContainer to="/">
+              <div className="logo-container">
+                <div className="logo">
+                  <svg height="100%" width="100%" xmlns="http://www.w3.org/2000/svg">
+                    <rect className="shape" height="100%" width="100%" />
+                  </svg>
+                  <p>Humaitek</p>
+                  <img className="normal icons" src={require('./assets/home_black.png')}></img>
+                  <img className="inverse icons" src={require('./assets/home_white.png')}></img>
                 </div>
-              </LinkContainer>
-              <LinkContainer to="contact"><div className="link line-hover">
-                <p>Contact</p>
-                <img className="icons" src={require('./assets/feedback.png')}></img>
-              </div></LinkContainer>
-            </div>
-          </Affix>
+              </div>
+            </LinkContainer>
+            <LinkContainer to="contact"><div className="link line-hover">
+              <p>Contact</p>
+              <img className="icons" src={require('./assets/feedback.png')}></img>
+            </div></LinkContainer>
+          </div>
           {this.props.children}
         </div>
     )
